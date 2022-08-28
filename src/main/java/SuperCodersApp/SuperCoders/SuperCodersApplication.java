a0af21a599e63fed39be045e039c3f598113243d
jac5341931 <jac5341@gmail.com>
2022-08-28 01:55:48 -0500

    Merge remote-tracking branch 'origin/development-julioCasanova' into development-julioCasanova

--- src/main/java/SuperCodersApp/SuperCoders/SuperCodersApplication.java
+++ src/main/java/SuperCodersApp/SuperCoders/SuperCodersApplication.java
@@ -16,20 +16,24 @@ public class SuperCodersApplication {
 
 		LocalDate ahora = LocalDate.now();
 		LocalDate ahora1 = LocalDate.now();
-		Enterprise aEnterprise = new Enterprise("MiEntidad", "CAlle 777", "3002326666", "8233584-3");
+		Enterprise aEnterprise = new Enterprise("Super Coders", "CAlle 777", "3002326666", "8233584-3");
 		Employee anEmployee = new Employee("Edward", "edwardrossiano@gmail.com");
 
 		Profile unProfile= new Profile("1",":)","3016703090","jac123");
 		Profile unProfile1= new Profile("2",":)","3016703091","Daniel");
 
-		Trasaction unaTransaction= new Trasaction(unProfile,
-				"se me fue la paloma",300000f,unProfile1,aEnterprise);
+		Trasaction unaTransaction= new Trasaction(unProfile.getId(), "se me fue la paloma",300000f,unProfile.getUser(),aEnterprise);
+		Trasaction una1Transaction= new Trasaction(unProfile1.getId(), "se me fue la paloma",300000f,unProfile1.getUser(),aEnterprise);
 
 		anEmployee.setEnterprise(aEnterprise);
 		anEmployee.setRole(Role.Admin);
 
 		System.out.println(aEnterprise.toString());
 		System.out.println(anEmployee.toString());
+		System.out.println(unProfile.toString());
+		System.out.println(unProfile1.toString());
+		System.out.println(unaTransaction.toString());
+		System.out.println(una1Transaction.toString());
 	}
 
 }
