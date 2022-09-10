be38c9ace6c35e28925676dce43729e23eb96e4c
JuanPAngulo <111084503+JuanPAngulo@users.noreply.github.com>
2022-09-09 21:51:46 -0500

    clase de TrasactionService

--- src/main/java/SuperCodersApp/SuperCoders/services/TrasactionService.java
+++ src/main/java/SuperCodersApp/SuperCoders/services/TrasactionService.java
@@ -11,17 +11,73 @@ import java.util.Optional;
 @Service
 public class TrasactionService implements ITrasactionService {
 
-    private ITrasactionRepository trasactionRepository;
-
-    public List<Trasaction> TrasactionService(ITrasactionRepository trasactionRepository) {
+    private final ITrasactionRepository trasactionRepository;
+    public Trasaction(ITrasactionRepository trasactionRepository) {
         this.trasactionRepository = trasactionRepository;
-
-
     }
 
 
 
+    public List<Trasaction> getAllTrasactions (){
+        return this.TrasactionRepository.findAll();
+    }
+
+    @Override
+    public List<Trasaction> getAllTrasactionS() {
+        return this.TrasactionRepository.findAll();
+    }
+
+    @Override
+    public Trasaction getTrasactionS(long id) {
+        Optional<Trasaction> e1 = this.trasactionRepository.findById(id);
+        return e1.orElse(null);
+    }
+
+    @Override
+    public Boolean createTrasactionS(Trasaction trasaction) {
+        try {
+            Trasaction.setId(01);
+            this.TrasactionRepository.save(trasaction);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean updateTrasactionS(Trasaction trasaction) {
+        try {
+            this.trasactionRepository.save(trasaction);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean deleteTrasactionSD(long id) {
+        try {
+            this.trasactionRepository.deleteById(id);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
