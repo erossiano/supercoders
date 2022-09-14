55e86ab8e9a1f6a6a07794d8a86cc160ff1f3045
ddnarvaez <ddnarvaez23@gmail.com>
2022-09-13 20:14:03 -0500

    index

--- src/main/java/SuperCodersApp/SuperCoders/controllers/EnterpriseController.java
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/EnterpriseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-<<<<<<< HEAD
+
 //Generaliza la ruta a todos los metodos
 @RequestMapping("enterprise")
 public class EnterpriseController {
@@ -51,42 +51,6 @@ public class EnterpriseController {
     @DeleteMapping("/{id}")
     public Boolean deleteEnterprise(@PathVariable("id") long id) {
         return this.enterpriseServices.deleteEnterpriseS(id);
-=======
-@RequestMapping("enterprise")
-public class EnterpriseController {
-    private final EnterpriseService enterpriseService;
 
-    public EnterpriseController(EnterpriseService enterpriseService) {
-        this.enterpriseService = enterpriseService;
-    }
-    @GetMapping
-    public List<Enterprise> getAllEnterprise(){
-        return this.enterpriseService.getAllEnterprise();
-    }
-    @GetMapping("/{id}")
-    public Enterprise getEnterprise (@PathVariable("id") long id) {
-        return this.enterpriseService.getEnterprise(id);
-    }
-    @PostMapping
-    public Boolean CreateEnterprise(@RequestBody Enterprise enterprise){
-        return this.enterpriseService.createEnterprise(enterprise);
-    }
-    @PatchMapping("/{id}")
-    public Boolean updateEnterprise(@PathVariable("id") long id,@RequestBody Enterprise enterprise){
-       Enterprise emp= this.enterpriseService.getEnterprise(id);
-       if(emp !=null){
-           try{
-               return this.enterpriseService.updateEnterprise(enterprise);
-           }catch(Exception e){
-               return false;
-           }
-       }else{
-           return false;
-       }
-    }
-    @DeleteMapping("/{id}")
-    public Boolean deleteEnterprise(@PathVariable("id") long id){
-        return this.enterpriseService.deleteEnterprise(id);
->>>>>>> origin/development-ddnarvaez
     }
 }
